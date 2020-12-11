50dbd371e5c0bc7f81f5bc9c038392e5de7075f0
Memet <meban2009@gmail.com>
2020-12-11 18:00:13 +0100

    Güncel hal US06 ve 07

--- src/test/java/runners/GmiBankRunner.java
+++ src/test/java/runners/GmiBankRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@gmibank",
+        tags = "@gmibank1",
         dryRun = false
 )
 public class GmiBankRunner {
