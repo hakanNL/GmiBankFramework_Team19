b1fd4c30cf234c630a4114546463baaa5076c937
iskendernuori <isknuori@gmail.com>
2020-12-12 23:01:08 +0200

    US14 test case ler hazırlandı pass alındı.

--- src/test/java/pages/employeePage/EmployeeNewAccountPage.java
+++ src/test/java/pages/employeePage/EmployeeNewAccountPage.java
@@ -1,4 +1,4 @@
-package pages.EmployeePage;
+package pages.employeePage;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,6 +18,7 @@ public class EmployeeNewAccountPage {
 
     @FindBy(id = "username")
     public WebElement username;
+
     @FindBy(id = "password")
     public WebElement password;
 
@@ -48,6 +49,58 @@ public class EmployeeNewAccountPage {
     public WebElement saveButton;
     @FindBy(xpath = "//*[@id=\"app-view-container\"]/div[1]/div/div/div/div[2]/div/form/div[1]/div/div")
     public WebElement invalidFeedback;
+
+    // US14 locator
+    @FindBy(xpath = "//*[@id=\"entity-menu\"]/div/a[1]/span")
+    public WebElement manageCustomers;
+
+    @FindBy(id = "jh-create-entity")
+    public WebElement createNewCustomer;
+    @FindBy(id = "search-ssn")
+    public WebElement ssnTexBox;
+    @FindBy(xpath = "//button[@class='btn btn-secondary']")
+    public WebElement searchButton;
+    @FindBy(id = "tp-customer-middleInitial")
+    public WebElement middle;
+    @FindBy(id = "tp-customer-phoneNumber")
+    public WebElement phone;
+    @FindBy(id = "tp-customer-zipCode")
+    public WebElement zipCode;
+    @FindBy(id = "tp-customer-city")
+    public WebElement ctiy;
+    @FindBy(id = "tp-customer-createDate")
+    public WebElement date;
+    @FindBy(xpath = "//*[@id=\"save-entity\"]/span")
+    public WebElement customerSave;
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[1]/div/div/div[1]")
+    public WebElement errorMessage;
+
+
+    @FindBy(xpath = "//div[@class='Toastify__toast Toastify__toast--success toastify-toast']")
+    public WebElement succesMessage;
+
+    @FindBy(id = "tp-customer-user")
+    public WebElement userDropDown;
+    @FindBy(id = "tp-customer-account")
+    public WebElement accountDropDown;
+    @FindBy(id = "tp-customer-zelleEnrolled")
+    public WebElement zelle;
+    @FindBy(id= "tp-account-createDate")
+    public WebElement createDateIcon;
+
+    @FindBy(xpath = "//span[.='Create Date']")
+    public WebElement createDateBox;
+    @FindBy(xpath = "//*[@id=\"account-menu\"]/a")
+    public WebElement outDropDown;
+    @FindBy(xpath = "//*[@id=\"account-menu\"]/div/a[3]/span")
+    public WebElement signOut;
+    @FindBy(id = "tp-customer-createDate")
+    public WebElement customerDate;
+
+
+
+
+
 
 
 
