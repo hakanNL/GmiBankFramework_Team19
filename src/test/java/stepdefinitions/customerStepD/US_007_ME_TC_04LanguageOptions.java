b9439e9003ef2441ccdfecaf815dec950f12279e
Memet <meban2009@gmail.com>
2020-12-12 21:49:53 +0100

    ilk yollama

--- src/test/java/stepdefinitions/customerStepD/US_007_ME_TC_04LanguageOptions.java
+++ src/test/java/stepdefinitions/customerStepD/US_007_ME_TC_04LanguageOptions.java
@@ -4,21 +4,25 @@ package stepdefinitions.customerStepD;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class US_007_ME_TC_04LanguageOptions {
     pages.customerPage.US_006_MEInfoSegmentPage infoPage = new pages.customerPage.US_006_MEInfoSegmentPage();
 
     @Then("Verify just English and Turkish option in the language list")
     public void verify_just_English_and_Turkish_option_in_the_language_list() {
-       System.out.println(infoPage.UserInfoLanguage.getText());
 
+        List<String> listActual = new ArrayList<>();
 
-        //List<String> list = new ArrayList<>();
-        //list.add(0,"English");
-        //list.add(1,"Türkçe");
-        //System.out.println(list);
-       // Assert.assertEquals(ReusableMethods.getElementsText(By.xpath("//select")), list);
-        Assert.assertTrue(infoPage.UserInfoLanguage.getText().contains("English") && infoPage.UserInfoLanguage.getText().contains("Türkçe"));
-        }
+        listActual.add(0, infoPage.english.getText());
+        listActual.add(1, infoPage.türkçe.getText());
+
+        List<String> listExpected = new ArrayList<>();
+        listExpected.add(0, "English");
+        listExpected.add(1, "Türkçe");
+        Assert.assertEquals(listExpected, listActual);
+    }
 
 }
 
