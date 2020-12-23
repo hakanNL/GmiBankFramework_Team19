5ce16d7fdaf60ef57ae0d0a2746368498f7ec46e
Memet <meban2009@gmail.com>
2020-12-23 15:59:16 +0100

    Us27 yazıldı

--- src/test/java/runners/GmiBankRunner.java
+++ src/test/java/runners/GmiBankRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@api",
+        tags = "@gmibank1",
         dryRun = false
 )
 public class GmiBankRunner {
